7af14e08cb766698737b5d5a834a6e7b0c359089
CHEN LEON <leon@fuba.tech>
2023-09-19 17:00:43 +0800

    add user curd,auth

--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -11,21 +11,29 @@ class UserAuthController extends Controller
     
     public function login(Request $request)
     {
-        $data = $request->all();
-        
-        if(Auth::attempt($data)){
-            $request->session()->regenerate();
+        $credentials = $request->all();
+        if(! $token = Auth::attempt($credentials)){
+            return response()->json([
+                'error code' => 1,
+            ],401);
         }
-        $user = Auth::user();
-
+        
         return response()->json([
-            'id' => $user,
-        ]);
+            'error code' => 0,
+            'jwt' => $token,
+        ],200);
     }
     
-
     public function logout(Request $request)
     {
+        Auth::logout();
+        return response()->json([
+            'error code' => 0
+        ],200);
+    }
 
+    public function refresh()
+    {
+        return $this->respondWithToken(Auth::refresh());
     }
 }
